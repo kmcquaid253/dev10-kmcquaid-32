18d7e805634db2cc0a8467ec4042b436b83e8214
unknown <kmcquaid253@gmail.com>
2022-08-25 22:01:27 +0100

    Updates

--- week-04/exercises/streams-exercises/src/main/java/learn/Main.java
+++ week-04/exercises/streams-exercises/src/main/java/learn/Main.java
@@ -1,7 +1,10 @@
 package learn;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,28 +15,81 @@ public class Main {
 
         // 0. Print all students
         // iteration solution
-        for (Student student : students) {
-            System.out.println(student);
-        }
+//        for (Student student : students) {
+//            System.out.println(student);
+//        }
 
         // stream solution
-        students.stream().forEach(System.out::println);
+        //students.stream().forEach(System.out::println);
 
         // 1. Print students from Argentina
 
+//        students.stream()
+//                .filter(student -> student.getCountry().equalsIgnoreCase("Argentina"))
+//                .forEach(System.out::println);
+
+
         // 2. Print students whose last names starts with 'T'.
 
+//        students.stream()
+//                .filter(student -> student.getLastName().startsWith("B")) // <-- this changed
+//                .forEach(System.out::println);
+
+
         // 3. Print students from Argentina, ordered by GPA
+//        students.stream()
+//                .filter(student -> student.getCountry().equalsIgnoreCase("Argentina"))
+//                .sorted(Comparator.comparing(Student::getGpa))
+//                .forEach(System.out::println);
+
+
 
         // 4. Print the bottom 10% (100 students) ranked by GPA.
 
+//        students.stream()
+//                .sorted(Comparator.comparing(Student::getGpa))
+//                .limit(100)
+//                .forEach(System.out::println);
+
+
+
         // 5. Print the 4th - 6th ranked students by GPA from Argentina
+//        students.stream()
+//                .filter(student -> student.getCountry().equalsIgnoreCase("Argentina"))
+//                .sorted(Comparator.comparing(Student::getGpa).reversed())
+//
+//                //.limit(6)
+//                .forEach(System.out::println);
+        //**************************************************************************************************
+
 
         // 6. Is anyone from Maldives?
 
+//        Optional<Student>  maldive = students.stream()
+//                .filter(student -> student.getCountry().equalsIgnoreCase("Maldives"))
+//                .findFirst();
+//
+//
+//        if (maldive.isPresent()) {
+//            Student maldiveStudent = maldive.get();
+//            System.out.println("Found a player from Maldive: " + maldiveStudent.getLastName() + ", " + maldiveStudent.getFirstName());
+//        } else {
+//            System.out.println("There are no players from Maldive.");
+//        }
+
         // 7. Does everyone have a non-null, non-empty email address?
+        students.stream()
+                .filter(student -> student.getEmailAddress().equals(0))
+
+                .forEach(System.out::println);
+
 
         // 8. Print students who are currently registered for 5 courses.
+
+//        students.stream()
+//                .filter(student -> student.getRegistrations().size() == 5)
+//                .forEach(System.out::println);
+
 
         // 9. Print students who are registered for the course "Literary Genres".
 
